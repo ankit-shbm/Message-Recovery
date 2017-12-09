077844d802f5cee6d68d5bd0c57c21322596de45
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-09 20:08:39 +0530

    Update Feistal_Preserving_Encryption.java

--- Feistal_Preserving_Encryption.java
+++ Feistal_Preserving_Encryption.java
@@ -2,6 +2,22 @@
 
 class Simple
 {
+   public class function(uint64_t key[], uint64_t tweak[], int m)
+   {
+   
+   }
+   
+    public static void EncryptedNumber(int A[], int n, uint64_t key[], uint64_t tweak[])             /* A[] is input bit-string.*/
+   {                                                                   
+      int i;
+      int m;                                                                                        /* Nos. of rounding to be done. */
+      System.Stdln.Input(m);
+      for(i = 0; i < m; i++)
+      {
+        function(key, tweak, m);
+      }     
+   }
+   
    public static void main(String[] args)                                                 /* Argc and Argv[] will lead to uint64_t functioning. */
    {
      int A[16];
@@ -11,10 +27,5 @@ class Simple
      uint64_t tweak[128];
      EncryptedNumber(A, n, key, tweak);
    }
-   public static void EncryptedNumber(int A[], int n, uint64_t key[], uint64_t tweak[])             /* A[] is input bit-string.*/
-   {                                                                   
-      int i;
-      for(i = 0; )
   
-   }
 }
