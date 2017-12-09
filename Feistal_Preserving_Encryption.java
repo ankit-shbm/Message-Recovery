b511931f891102c4ba2c31a3d240a93ed666ea88
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-09 18:21:19 +0530

    Update Feistal_Preserving_Encryption.java

--- Feistal_Preserving_Encryption.java
+++ Feistal_Preserving_Encryption.java
@@ -1,7 +1,16 @@
 /***** This is the time for Java .  ******/
 
-public static void EncryptedNumber(int A[], int n, int )       /* A[] is input bit-string.*/
+class Simple
+{
+   public static void main(String[] args)
+   {
+     int A[16];
+     int n;
+     System.in.println(n);
+   }
+public static void EncryptedNumber(int A[], int n)             /* A[] is input bit-string.*/
 {                                                              /* n is length at which string to be broken. */ 
   
   
+}
 }
