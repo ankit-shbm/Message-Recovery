a9d798bd49cc1c37086f5b30ce2badcd034d5381
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-09 18:56:55 +0530

    Update Feistal_Preserving_Encryption.java

--- Feistal_Preserving_Encryption.java
+++ Feistal_Preserving_Encryption.java
@@ -2,15 +2,19 @@
 
 class Simple
 {
-   public static void main(String[] args)
+   public static void main(String[] args)                        /* Argc and Argv[] will lead to uint64_t functioning. */
    {
      int A[16];
      int n;
      System.in.println(n);
+     uint64_t key[n] ; 
+     uint64_t tweak[n];
+     EncryptedNumber(A, n, key, tweak);
    }
-public static void EncryptedNumber(int A[], int n)             /* A[] is input bit-string.*/
-{                                                              /* n is length at which string to be broken. */ 
+   public static void EncryptedNumber(int A[], int n)             /* A[] is input bit-string.*/
+   {                                                              /* n is length at which string to be broken. */ 
+     int i;
+      for(i = 0; )
   
-  
-}
+   }
 }
