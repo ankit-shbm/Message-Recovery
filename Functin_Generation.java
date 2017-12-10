60d9f48982962da7aabc206126d4e2122de39fbe
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-10 19:26:58 +0530

    Update Functin_Generation.java

--- Functin_Generation.java
+++ Functin_Generation.java
@@ -4,26 +4,27 @@
 
 public class function(uint64_t key, uint64_t tweak, int i, int n, uint64_t EncryptedSide, int BlockSize)
    {
-      int i, padding;                                  
-      uint64_t key1[n];                                /* String store. */
+      int i, padding, padding1;                                  
+      uint64_t key1[n], key2[n];                                /* String store. */
       padding = key % n;                               /* Check the length to be padded. */
       for(i = n - padding; i < '\0'; i++)
       {
         static int count = 0;
         key1[count] = key[i];                         /* Last Bits of strings stored. */
+        count++;
       }
       for(i = 0; i <= padding; i++)
       {
         key1 = '0' + 'key1';                          /* Concatenating with 0's. Now, key1 is multiple of 'n'. */
       }
       count = 0;
-      padding = (BlockSize - n) % n;
+      padding1 = (BlockSize - n) % n;
       for(i = n - padding; i < '\0'; i++)
       {
-        static int count;
         key2[count] = EncryptedSide[i];
+        count++;
       }
-      for(i = 0; i <= padding; i++)
+      for(i = 0; i <= padding1; i++)
       {
         key1 = '0' + 'key1';                          /* Concatenating with 0's. Now, key1 is multiple of 'n'. */
       }
