b2b013d4812794623004f48470cbc73e17d1af3c
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-10 17:13:30 +0530

    Update Functin_Generation.java

--- Functin_Generation.java
+++ Functin_Generation.java
@@ -1,1 +1,6 @@
+/***** Generation of Random Function before XOR with the parsed other side block. *****l
 
+   public class function(uint64_t key[], uint64_t tweak[], int i, int n, uint64_t EncryptedSide)
+   {
+     return  
+   }
