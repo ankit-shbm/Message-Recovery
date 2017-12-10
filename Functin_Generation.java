592d0fe364f76b461cf41c0f5d9a40e9eb936230
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-10 20:48:45 +0530

    Update Functin_Generation.java

--- Functin_Generation.java
+++ Functin_Generation.java
@@ -9,25 +9,25 @@ public class function(uint64_t key, uint64_t tweak, int i, int n, uint64_t Encry
    
       padding = strlen(key) % n;                               /* Check the length to be padded. */
       if(strlen(key) > n)
-      {
-      for(i = n - padding; i < '\0'; i++)
-      {
-        static int count = 0;
-        key1[count] = key[i];                         /* Last Bits of strings stored. */
-        count++;
-      }
-      for(i = 0; i <= padding; i++)
-      {
-        key1 = '0' + key1;                          /* Concatenating with 0's. Now, key1 is multiple of 'n'. */
-      }
-      }
+          {
+             for(i = strlen(key) - padding; i < strlen(key); i++)
+                {
+                   static int count = 0;
+                   key1[count] = key[i];                         /* Last Bits of strings stored. */
+                   count++;
+                }
+             for(i = 0; i < n - padding; i++)
+                {
+                   key1 = '0' + key1;                          /* Concatenating with 0's. Now, key1 is multiple of 'n'. */
+                }
+          }
       else
-      {
-        for(i = n - padding; i < n; i++)
-        {
-          key1 = '0' + key;
-        }
-      }
+         {
+             for(i = n - padding; i < n; i++)
+                {
+                   key1 = '0' + key;
+                }
+         }
    
       count = 0;
       padding1 = (BlockSize - n) % n;
@@ -62,14 +62,14 @@ public class function(uint64_t key, uint64_t tweak, int i, int n, uint64_t Encry
       }
       for(i = 0; i <= padding1; i++)
       {
-        key2 = '0' + 'key2';                         /* Concatenating with 0's. Now, key2 is multiple of 'n'. */
+        key2 = '0' + key2;                         /* Concatenating with 0's. Now, key2 is multiple of 'n'. */
       }
       }
       else
       {
         for(i = n - padding2; i < n; i++)
         {
-          key2 = '0' + key2;
+          key2 = '0' + ;
         }
       }
    }
