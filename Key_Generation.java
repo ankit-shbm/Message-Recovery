49406c296d41839a3577fabba2a85cf80734d4c3
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-12 16:48:17 +0530

    Update Key_Generation.java

--- Key_Generation.java
+++ Key_Generation.java
@@ -1,1 +1,13 @@
 /***** Generation of a set with combination of bit-string with each of 128-bit. *****/ 
+/* Total nos. of key-generated is equivalent to pow(m, n). */
+/* 'm' is the total nos. of variable to be used. */
+/* 'n' is the length of the string of the key that to be generated. */
+/* For the given set, n = 128 and m = 2. A[m] = {0, 1}. */
+
+public class char KeyGeneration()
+{
+    
+}
+  
+  
+  
