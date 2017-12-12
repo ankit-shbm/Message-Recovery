447b739081a715f78be0e4b33f1fdc9750f14164
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-12 23:44:03 +0530

    Update Key_Generation.java

--- Key_Generation.java
+++ Key_Generation.java
@@ -4,9 +4,21 @@
 /* 'n' is the length of the string of the key that to be generated. */
 /* For the given set, n = 128 and m = 2. A[m] = {0, 1}. */
 
-public class char KeyGeneration()
+public class char KeyGeneration(char[] key)
 {
-    
+    int i = 0;
+    String[] Key_Generated;
+    Key_Generated[0] = '0';
+    Key_Generated[1] = '1';
+    int count = 1;
+    while(count < 128)
+     {
+        while(count < count + 1)
+        {
+          Key_Generated[i] = strcat('0');
+        }
+        count++;
+     }
 }
   
   
