ee42d7359edca79436b859ecc3b77389055e00c2
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-14 11:36:05 +0530

    Update Key_Generation.java

--- Key_Generation.java
+++ Key_Generation.java
@@ -17,17 +17,17 @@ public class KeyGeneration
     }
     public static void main(String args[])
     {
-       static int i = 2;
+       int i = 2;
        String Key_Generated[] = new String[pow(2, 128)];                         /* Max. size allocation of Array.*/
        Key_Generated[0] = '0';
        Key_Generated[1] = '1';
-       for(static int j = 0; j < Key_Generated.length; j++)                      /* Dynamic allocation of sizeof Key_Generated Array */
+       for(double j = 1; j < Key_Generated.length; j++)                      /* Dynamic allocation of sizeof Key_Generated Array */
         {
            if(Key_Generated.length < pow(2, 128))
              {
-                Key_Generated[i] = concate(Key_Generated[j], Key_Generated[0]);  /* String concatenation with 0's. */
+                Key_Generated[i] = cate(Key_Generated[j], Key_Generated[0]);  /* String concatenation with 0's. */
                 i++;
-                Key_Generated[i] = concate(Key_Generated[j], Key_Generated[0]);  /* String concatenation with 1's. */
+                Key_Generated[i] = cate(Key_Generated[j], Key_Generated[1]);  /* String concatenation with 1's. */
                 i++;
              }
            else
@@ -35,7 +35,12 @@ public class KeyGeneration
                break;
              }
         }
- }
-
-
-
+      for(i = 0; i < Key_Generated.length; i++)
+        {
+           while(length(Key_Generated[i] < 128))
+             {
+                 cate( '0', Key_Generated[i]);                                 /* Conversion into 128 bits-string. */
+             }
+        }
+    }
+}
