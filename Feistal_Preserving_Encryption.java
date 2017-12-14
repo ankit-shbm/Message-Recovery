412ec7907218bea6ce3cfebdb2e603d7343fcd97
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-14 11:40:48 +0530

    Update Feistal_Preserving_Encryption.java

--- Feistal_Preserving_Encryption.java
+++ Feistal_Preserving_Encryption.java
@@ -2,12 +2,49 @@
 
 class Simple
 {
-   public class function(uint64_t key[], uint64_t tweak[], int i, int n, uint64_t EncryptedSide)
-   {
-     
-   }
+   static String function(String key, String tweak, int k, int n, String EncryptedSide, int BlockSize)
+   {   
+                                                                           /* Function-Key generation. */
+     String FunctionOut;
+     int padding, padding_;
+     key = key * k;
+     EncryptedSide = EncryptedSide * k;
+     EncryptedSide = ("\0", key, EncryptedSide); 
+     FunctionOut = BinaryConversion(EncryptedSide);
+     FunctionOut = strcat(FunctionOut, tweak);
+     padding_ = length(FunctionOut);
+     padding = padding_ % n;
    
-   public class EncryptedNumber(int A[], int n, uint64_t key[], uint64_t tweak[])             /* A[] is input bit-string.*/
+     if(padding_ > n)
+      {
+          for(i = 0; i < n - padding; i++)
+             {
+                 FunctionOut = FunctionOut +  '0';                         /* Concatenating with 0's. Now, key2 is multiple of 'n'. */
+             }
+      }   
+   
+     int l, count = 0, m;
+     padding_ = length(FunctionOut);
+     String Function;
+     m = 2 * n;
+    /* Parsing the Bit-String into multiple of n-length. */
+     for(i = 0; i < n; i++)
+     {
+        Function[i] = XOR(FunctionOut[i], FunctionOut[m + i]);
+     }
+    while(i < padding_)
+    {
+      int j = 0;
+      for(i = m; i < m + n; i++)
+      {
+        Function[j] = XOR(Function[j], FunctionOut[i]);
+        j++;
+      }
+      m = m + n;
+    }
+ }
+   
+   static String EncryptedNumber(int A[], int n, uint64_t key[], uint64_t tweak[])             /* A[] is input bit-string.*/
    {                                                                   
       int i;
       int m;                                                                                        /* Nos. of rounding to be done. */
