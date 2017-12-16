71c9773ddcb1069dd6e9d160eee844fc36076cd1
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-16 12:42:17 +0530

    Update Simple_P_E_S_scheme.java

--- Simple_P_E_S_scheme.java
+++ Simple_P_E_S_scheme.java
@@ -2,7 +2,7 @@
 
 class Simple
 {
-    static int pow(int a, int b)
+    static double pow(int a, int b)
     {
       double num = 1;                                                            /* Double for more than 32 bits. */
         for(int i = 0; i < b; i++)
@@ -24,26 +24,26 @@ class Simple
         }
    }
    
-   static String function(String key, String tweak, int k, int n, String EncryptedSide, int BlockSize)
+   static String function(String key, String tweak, int k, int n, double EncryptedSide)
    {   
                                                                            /* Function-Key generation. */
      String FunctionOut = new String();
      int padding, padding_;
      EncryptedSide = EncryptedSide * k;
      EncryptedSide = BinaryConversion(EncryptedSide);
-     key = key.concat(EncryptedSide); 
+     String EncryptedSide1 = new String(EncryptedSide);
+     key = key.concat(EncryptedSide1); 
      FunctionOut = key.concat(tweak);
-     padding_ = length(FunctionOut);
+     padding_ = FunctionOut.length;
      padding = padding_ % n;
    
-     for(i = 0; i = n - padding; i++)
+     for(int i = 0; i = n - padding; i++)
        {
              FunctionOut = FunctionOut.concat("0");                         /* Concatenating with 0's. Now, key2 is multiple of 'n'. */ 
        }  
    
      int l, count = 0, m;
-     padding_ = length(FunctionOut);
-     char[] Function = new char[128];
+     int[] Function = new int[128];
      m = 2 * n;
      
     /* Parsing the Bit-String into multiple of n-length. */
@@ -52,7 +52,11 @@ class Simple
       {
         for(i = 0; i < n; i++)
           {
-             Function[i] = XOR(FunctionOut[i], FunctionOut[m + i]);
+             int FunctionOut1 = new int[1];
+             int FunctionOut2 = new int[1];
+             FunctionOut1 = new int(FunctionOut[i]);
+             FunctionOut2 = new int(FunctionOut[m + i]);
+             Function[i] = XOR(FunctionOut1, FunctionOut2);
           }
       }
     while(i < padding_)
@@ -60,7 +64,9 @@ class Simple
       int j = 0;
       for(i = m; i < m + n; i++)
       {
-        Function[j] = XOR(Function[j], FunctionOut[i]);
+        int FunctionOut1 = new int[1];
+        FunctionOut1 = new int(FunctionOut[i]);
+        Function[j] = XOR(Function[j], FunctionOut1);
         j++;
       }
       m = m + n;
@@ -85,7 +91,7 @@ class Simple
      }
      s1.number = num % b;                                                            /* Mod with respect to Array_Size. */
    }
-   static void Key_Permutation(String[] Key_Generated, int Round, double a, double b, )
+   static void Key_Permutation(String[] Key_Generated, int Round, double a, double b )
      {
         modulus(a, Round, b, s1);
         s1.number = Key_Generated[s1.number];                                       /* s1.number is the Returned KeyPermuted value.*/
@@ -98,47 +104,58 @@ class Simple
 //    }    
 
     
-   static String EncryptedNumber(int A[], int n, double key[], double tweak[])             /* A[] is input bit-string.*/
+   static String EncryptedNumber(double A[], int n, int i, double key[], double tweak[])             /* A[] is input bit-string.*/
    {                                                                   
-      int i;
-      int m;                                                                                        /* Nos. of rounding to be done. */
-      
-
-      /*** String Parsing in Right Encrypted and LeftEncrypted. ***/
-      
+      int m;                                     /* nos. of rounds. */                                                                                        /* Nos. of rounding to be done. */
+      int[] LeftEncrypted1 = new int[n];
+      int[] RightEncrypted1 = new int[64 - n];
+      for(int j = 0; j < n; j++)
+        {
+           LeftEncrypted1[j] = A[j];
+        }
+      for(int j = 0; j < 64 - n; j++)
+        {
+           RightEncrypted1[j] = A[j + n];
+        }
+      String LeftEncrypted = new String(LeftEncrypted1);
+      String RightEncrypted = new String(RightEncrypted1);
       for(i = 0; i < m; i++)
       {                                                                         
         if(i%2 == 0)
         {
-          LeftEncrypted[i + 1] = XOR(LeftEncrypted[i], function(key, tweak, i, n, RightEncrypted[i]));               /* Each Round new function Generation with respect to RightEncrypted. */
-          RightEncrypted[i + 1] = RightEncrypted[i];
+          LeftEncrypted = XOR(LeftEncrypted, function(key, tweak, i, n, RightEncrypted));               /* Each Round new function Generation with respect to RightEncrypted. */
         }
         else
         {
-          RightEncrypted[i + 1] = XOR(RightEncrypted[i], function(key, tweak, i, n, LeftEncrypted[i]));               /* Each Round new function Generation with respect to LeftEncrypted.. */                                        
-          LeftEncrypted[i + 1] = LeftEncrypted[i];
+          RightEncrypted = XOR(RightEncrypted, function(key, tweak, i, n, LeftEncrypted));               /* Each Round new function Generation with respect to LeftEncrypted.. */                                        
         }
       }     
    }
    
    public static void main(String[] args)                                              /* Argc and Argv[] will lead to uint64_t functioning. */
    {
-     int A[16];
+     int A[] = new int[16];                                                            /* */
      int n;                                                                            /* n is length at which string to be broken. */
-     double key[128];                                                                /* An unique random key from the Generated set. */
-     double tweak[128];                                                              /* An unique random tweak from the Generated set. */
+     String key = new String();                                                                /* An unique random key from the Generated set. */
+     String tweak = new String();                                                    /* An unique random tweak from the Generated set. */
+     power s1 = new power();                                                     /* s1 assigned as object named power. */
      EncryptedNumber(A, n, key, tweak);
       int i = 2;
-       String Key_Generated[] = new String[pow(2, 128)];                         /* Max. size allocation of Array.*/
+       char Key_Generated[] = new char[pow(2, 128)];                         /* Max. size allocation of Array.*/
        Key_Generated[0] = '0';
        Key_Generated[1] = '1';
        for(double j = 1; j < Key_Generated.length; j++)                      /* Dynamic allocation of sizeof Key_Generated Array */
         {
            if(Key_Generated.length < pow(2, 128))
              {
-                Key_Generated[i] = cate(Key_Generated[j], Key_Generated[0]);  /* String concatenation with 0's. */
+                String k1 = new String();
+                k1 = Key_Generated[j];
+                k1 = k1.concat(Key_Generated[0]);
+                Key_Generated[i] = k1;  /* String concatenation with 0's. */
                 i++;
-                Key_Generated[i] = cate(Key_Generated[j], Key_Generated[1]);  /* String concatenation with 1's. */
+                k1 = Key_Generated[j];
+                k1 = k1.concat(Key_Generated[1]);
+                Key_Generated[i] = k1;  /* String concatenation with 1's. */
                 i++;
              }
            else
@@ -148,15 +165,16 @@ class Simple
         }
        for(i = 0; i < Key_Generated.length; i++)
         {
-           if(length.(Key_Generated[i]) < 128)
+           k1 = Key_Generated[i];
+           if(k1.length < 128)
              {
                  String zero = new String();
                  zero = "0";
-                 for(j = 1; j < 128 - length.(Key_Generated[i]); j++)
+                 for(j = 1; j < 128 - k1.length; j++)
                  {
                      zero = zero.concat("0");
                  }
-                 Key_Generated[i] = zero.concat(Key_Generated[i]);                                 /* Conversion into 128 bits-string. */
+                 Key_Generated[i] = zero.concat(k1);                                 /* Conversion into 128 bits-string. */
              }
         }    
    }
